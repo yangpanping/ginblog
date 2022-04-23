84e2deb8d7863d63c73fd00b1535fac9afd7ff82
YangPanPing <1402187756@qq.com>
2022-04-23 10:00:25 +0800

    实现堆排序

--- ts/datastruct/datastruct.go
+++ ts/datastruct/datastruct.go
@@ -95,6 +95,41 @@ func selectSort(datas []int) {
 	}
 }
 
+//堆排序
+func HeapSortMax(arr []int, length int) []int {
+	// length := len(arr)
+	if length <= 1 {
+		return arr
+	}
+	depth := length/2 - 1 //二叉树深度
+	for i := depth; i >= 0; i-- {
+		topmax := i //假定最大的位置就在i的位置
+		leftchild := 2*i + 1
+		rightchild := 2*i + 2
+		if leftchild <= length-1 && arr[leftchild] > arr[topmax] { //防止越过界限
+			topmax = leftchild
+		}
+		if rightchild <= length-1 && arr[rightchild] > arr[topmax] { //防止越过界限
+			topmax = rightchild
+		}
+		if topmax != i {
+			arr[i], arr[topmax] = arr[topmax], arr[i]
+		}
+	}
+	return arr
+}
+func HeapSort(arr []int) []int {
+	length := len(arr)
+	for i := 0; i < length; i++ {
+		lastlen := length - i
+		HeapSortMax(arr, lastlen)
+		if i < length {
+			arr[0], arr[lastlen-1] = arr[lastlen-1], arr[0]
+		}
+	}
+	return arr
+}
+
 func main() {
 	data := []int{49, 38, 65, 97, 76, 13, 27, 49, 11, 23, 56, 97, 22, 54, 11, 45}
 
@@ -102,6 +137,7 @@ func main() {
 	//insertsort2(data)
 	//bubbleSort(data)
 	//quickSort(data, 0, len(data)-1)
-	selectSort(data)
-	fmt.Println(data)
+	//selectSort(data)
+
+	fmt.Println(HeapSort(data))
 }
